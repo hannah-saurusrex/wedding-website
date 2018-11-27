ef301619f285856ee751c7c06f2b80418e2e8397
Hannah McMahon <hrose.mcmahon@gmail.com>
2018-11-27 13:50:34 -0700

    tried putting the script in the js file; removed when it didn't work

--- js/main.js
+++ js/main.js
@@ -4,14 +4,13 @@
   const topOfNav = nav.offsetTop;
   
   function fixNav() {
-    console.log(topOfNav);
-    // if(window.scrollY >= topOfNav) {
-    //   document.body.style.paddingTop = nav.offsetHeight + 'px';
-    //   document.body.classList.add('fixed-nav');
-    // } else {
-    //   document.body.style.paddingTop = 0;
-    //   document.body.classList.remove('fixed-nav');
-    // }
+    if(window.scrollY >= topOfNav) {
+      document.body.style.paddingTop = nav.offsetHeight + 'px';
+      document.body.classList.add('fixed-nav');
+    } else {
+      document.body.style.paddingTop = 0;
+      document.body.classList.remove('fixed-nav');
+    }
   }
   
   window.addEventListener('scroll', fixNav);
